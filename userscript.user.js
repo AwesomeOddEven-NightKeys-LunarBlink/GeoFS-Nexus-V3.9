ff1dec5c0ed491714bc1148b1ef0c04e147d37ed
Even Gao <even@Evens-MacBook-Pro.local>
2026-04-03 13:13:14 +0800

    Revert to dynamic injection and add cachebuster

--- userscript.user.js
+++ userscript.user.js
@@ -1,12 +1,18 @@
 // ==UserScript==
 // @name         GeoFS-Nexus-V3.9
 // @namespace    http://tampermonkey.net/
-// @version      1.0.1
+// @version      1.0.2
 // @description  26+Addons, with detailed procedures and improved UI!
 // @author       AwesomeOddEven-NightKeys-LunarBlink
 // @match        https://www.geo-fs.com/geofs.php?v=*
 // @match        https://*.geo-fs.com/geofs.php*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=geo-fs.com
-// @require      https://raw.githack.com/AwesomeOddEven-NightKeys-LunarBlink/GeoFS-Nexus-V3.9/main/main.js
 // @grant        none
 // ==/UserScript==
+(() => {
+    // Dynamic injection ensures Tampermonkey doesn't permanently cache your development code.
+    var ts = Math.floor(Date.now() / 60000); // 1-minute cache buster
+    var addonScript = document.createElement('script'); 
+    addonScript.src="https://raw.githack.com/AwesomeOddEven-NightKeys-LunarBlink/GeoFS-Nexus-V3.9/main/main.js?t=" + ts;
+    document.body.appendChild(addonScript);
+})();
