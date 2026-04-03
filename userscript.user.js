b4f860dce4c4bb3deb383ea72f7ae5c2359ec435
Even Gao <even@Evens-MacBook-Pro.local>
2026-04-03 12:59:16 +0800

    Rewrite userscript to use native @require for main.js loading

--- userscript.user.js
+++ userscript.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         GeoFS-Nexus-V3.9
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
+// @version      1.0.1
 // @description  26+Addons, with detailed procedures and improved UI!
 // @author       AwesomeOddEven-NightKeys-LunarBlink
 // @match        https://www.geo-fs.com/geofs.php?v=*
 // @match        https://*.geo-fs.com/geofs.php*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=geo-fs.com
+// @require      https://raw.githack.com/AwesomeOddEven-NightKeys-LunarBlink/GeoFS-Nexus-V3.9/main/main.js
+// @grant        none
 // ==/UserScript==
-(() => {var addonScript = document.createElement('script'); addonScript.src="https://raw.githack.com/AwesomeOddEven-NightKeys-LunarBlink/GeoFS-Nexus-V3.9/main/main.js";document.body.appendChild(addonScript);})();
